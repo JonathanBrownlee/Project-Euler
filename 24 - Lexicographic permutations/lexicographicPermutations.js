bc483880fe99e095561bfa6159ceefc8a05e5101
Jonathan <jonathan@nightlife.com.au>
2019-05-07 10:31:02 +1000

    Added js soln to 24

---
+++ 24 - Lexicographic permutations/lexicographicPermutations.js
@@ -0,0 +1,26 @@
+var main = function (num, i) {
+    var perms = getAllPerms(num).sort();
+    return perms[i];
+}
+
+var getAllPerms = function (num) {
+    var arr = [];
+
+    if (num.length == 1) {
+        arr.push(num);
+        return num;
+    }
+
+    for (var i = 0; i < num.length; i++) {
+        var first = num[i];
+        var whatsLeft = num.substring(0, i) + num.substring(i + 1);
+        var perms = getAllPerms(whatsLeft);
+        for (var j = 0; j < perms.length; j++) {
+            arr.push(first + perms[j])
+        }
+    }
+    return arr;
+}
+
+
+console.log(main('0123456789', 999999));
