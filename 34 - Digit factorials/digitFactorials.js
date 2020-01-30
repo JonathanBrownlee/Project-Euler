c6c576ddc6cd07d99e83b146a9c64c96b6e79080
jbrownlee <v-jbrownlee@expedia.com>
2020-01-30 17:12:48 +1000

    Added js soln to 34

---
+++ 34 - Digit factorials/digitFactorials.js
@@ -0,0 +1,24 @@
+var main = () => {
+    var total = 0;
+    for (var i = 3; i < 1000000; i++) {
+        var temp = i.toString()
+            .split("")
+            .map((item) => factorialOf(parseInt(item, 10)))
+            .reduce((accumulator, currentValue) => accumulator + currentValue);
+        if (i === temp) {
+            console.log(i);
+            total += temp;
+        }
+    }
+
+    return total;
+};
+
+var factorialOf = (num) => {
+    var current = 1;
+    for (var i = 2; i <= num; i++)
+        current = current * i;
+    return current;
+};
+
+console.log(main());
